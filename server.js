dbbfd3f8392edac107d4e415c2fdbca999e1c345
Aj <ajanthas@presidio.com>
2023-06-06 21:35:32 +0530

    changed port

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get("/", (req, res) => {
 });
 require("./app/routes/tutorial.routes")(app);
 // set port, listen for requests
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
